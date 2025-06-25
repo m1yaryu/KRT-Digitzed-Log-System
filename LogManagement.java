954c87f16580968827e841a060f25cc0b5b5d230
m1yaryu <127284546+m1yaryu@users.noreply.github.com>
2025-06-25 12:50:54 +0900

    Slowly integrating table into main logic
    removed text area in setui logic
    added table logic in file handling etc

--- LogManagement.java
+++ LogManagement.java
@@ -1,9 +1,16 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FilterWriter;
+import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.annotation.processing.FilerException;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 
@@ -32,19 +39,57 @@ public class LogManagement {
         return newFile;
     }
 
-    public static void appendLogEntry(String[] labels, String[] data, JTextArea textArea) throws IOException {
-        StringBuilder entry = new StringBuilder();
+    public static Object[] tableData() {
+        String[] columnNames = null;
+        List<String[]> rowList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("logEntries.txt"))) {
+            String line;
+            boolean isFirstLine = true;
 
+            while((line = br.readLine()) != null) {
+                String[] values = line.split("\t\t");
+                if(isFirstLine) {
+                    columnNames = values;
+                    isFirstLine = false;
+                }
+                else {
+                    rowList.add(values);
+                }
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        String[][] data = rowList.toArray(new String[0][]);
+
+        return new Object[] {columnNames, data};
+    }
+
+    public static void appendLogEntry(String[] labels, String[] data, String entryLabel, JFrame parentFrame) throws IOException {
+        StringBuilder entryBuilder = new StringBuilder();
+
+        //header
         String timeStamp = new SimpleDateFormat("HHmm'H'").format(new Date());
-        entry.append(timeStamp).append("\t");
+        entryBuilder.append(timeStamp).append("\t");
+        entryBuilder.append(entryLabel).append("\t");
 
-        entry.append(String.format("%-23s %s\n", labels[0], data[0]));
+        entryBuilder.append(String.format("%-23s %s\n", labels[0], data[0]));
         for(int i=1; i<labels.length; i++){
-            entry.append("\t");
-            entry.append(String.format("%-23s %s\n", labels[i], data[i]));
+            entryBuilder.append(String.format("%-23s %s\n", labels[i], data[i]));
         }
         
-        entry.append("\n");
-        textArea.append(entry.toString());
+        entryBuilder.append("[foobar]");
+
+        String entry = entryBuilder.toString();
+        FileWriter writer = new FileWriter("logEntries.txt");
+        try(writer){
+            writer.write(entry);
+            JOptionPane.showMessageDialog(parentFrame, "Changes saved successfulyly!", "Success", JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch(Exception e) {
+            JOptionPane.showMessageDialog(parentFrame, "Error saving file: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
     }
 }
